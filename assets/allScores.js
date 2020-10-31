c817631b5d8505a0e56cc6de8956757a0e076d15
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-10-31 15:56:29 -0700

    assets/allScores.js

--- assets/allScores.js
+++ assets/allScores.js
@@ -2,8 +2,13 @@
 var newOl = document.querySelector("#highScores");
 var newLi = document.createElement("li");
 
-// create a Clear High Scores button
-
-// create a Go Back Button
 
 // create elements to store and add new scores
+function renderHighScores() {
+    // get scores from local storage or an empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+}
+
+
+// create a Clear High Scores button
