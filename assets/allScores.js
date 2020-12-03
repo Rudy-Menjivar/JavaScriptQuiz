0aa20c074de20851935021dc424df06d0da2c647
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-12-03 14:05:06 -0800

    ren OL el & worked on formatting

--- assets/allScores.js
+++ assets/allScores.js
@@ -2,7 +2,6 @@
 function renderHighScores() {
     // get scores from local storage or an empty array
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-
     // sort highscores
     highscores.sort(function(a, b) {
         return b.score - a.score;
@@ -11,11 +10,10 @@ function renderHighScores() {
     highscores.forEach(function(score) {
         // li tag will list last score with initials
         var newLi = document.createElement("li");
-        newLi.textContent = score.initials + " - " + score.score;
-        
+        newLi.textContent = score.initials + " - " + score.score;    
         // Append new li tag to OL highScores
-        var topScores = document.querySelector("#highScores");
-        topScores.appendChild(newLi);
+        var scoresOL = document.querySelector("#highScores");
+        scoresOL.appendChild(newLi);
     });
 }
 // Clear high scores with function
@@ -23,9 +21,7 @@ function clearHighScores() {
     window.localStorage.removeItem("highscores");
     window.location.reload();
 }
-
 // Clear High Scores with onclick (and function)
 document.querySelector("#clearHighScores").onclick = clearHighScores;
 
-// Run renderHighScores when page loads
-renderHighScores();
+renderHighScores(); // Run renderHighScores when page loads
